4624091dc7e7b06bf231dcdbaa990a5bab18064d
soda-ronnie <ronnie.yang@sodastrategic.com.au>
2021-02-11 15:40:18 +1100

    Updated with org info

--- app.js
+++ app.js
@@ -2,19 +2,20 @@
 	set this as your instance url appended with /cometd/40.0
 	example: https://na10.salesforce.com/cometd/40.0
 */
-let baseUrl = 'https://rogermitchell-dev-ed.my.salesforce.com/cometd/40.0';
+let baseUrl = "https://suttontools--full.my.salesforce.com/cometd/40.0";
 
 /*
 	either make an OAuth call and retrieve the token,
 	or copy this value from the `sid` cookie associated
 	with your baseUrl from within the browser
 */
-let token = '00DE00000001233333!jfdsfkljdsklfjdasjflds;fjkadsfaddasf';
+let token =
+    "00D0T0000008arm!AQoAQODS1jJ1o.fUoQ__KnM5h50jTmj1NgD1VQgtCtg5rjSInEQAvB1dyAHVH5vD5_bfOUI75VCx9YDP33Ju4_IZs_7HXIlT";
 
 /*
 	set this as the API name of your platform event
 */
-let platformEventName = 'User_Update__e';
+let platformEventName = "New_S3_Asset__e";
 
 // define node modules
 require('cometd-nodejs-client').adapt();
